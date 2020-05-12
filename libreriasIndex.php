f7baa3e080993c77bc3a208d87cbe0b2e510d2cc
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-12 14:53:44 -0500

    Update libreriasIndex.php

--- libreriasIndex.php
+++ libreriasIndex.php
@@ -1,6 +1,5 @@
-
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
-<link rel="stylesheet" type="text/css" href="js/jquery-ui.min.css">
+<link rel="stylesheet" type="text/css" href="js/jquery-ui.css">
 <link rel="stylesheet" type="text/css" href="lib/bootstrap/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="lib/dataTables/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="lib/dataTables/dataTables.bootstrap4.min.css">
@@ -11,11 +10,7 @@
 <link rel="stylesheet" type="text/css" href="css/estilos_index.css">
 
 
-<!--<script src="js/jquery-1.10.2.js"></script> para autocomplete-->
-<!--<script src="js/jquery-ui.js"></script> para autocomplete-->  
-<!--<script src="js/jquery.min.js"></script>-->
-<script src="js/jquery-3.2.0.min.js"></script>
-<script src="js/jquery-ui.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="lib/bootstrap/popper.min.js"></script>
 <script src="lib/bootstrap/bootstrap.min.js"></script>
 <script src="lib/dataTables/jquery.dataTables.min.js"></script>
