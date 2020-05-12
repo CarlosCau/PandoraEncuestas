742c8ca42fcde185ce8334bf5b54cdbec970e8fb
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-11 23:29:20 -0500

    Update librerias.php

--- librerias.php
+++ librerias.php
@@ -1,4 +1,3 @@
-
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
 <link rel="stylesheet" href="css/leaflet.css">
 <link rel="stylesheet" type="text/css" href="js/jquery-ui.css">
@@ -19,9 +18,11 @@
 <link rel="stylesheet" href="lib/plugins/leaflet-search.css">
 
 
-<script src="js/jquery-1.10.2.js"></script> <!--para autocomplete-->
-<script src="js/jquery-ui.js"></script> <!--para autocomplete-->  
+<!--<script src="js/jquery-1.10.2.js"></script> para autocomplete-->
+<!--<script src="js/jquery-ui.js"></script> para autocomplete-->  
 <!--<script src="js/jquery.min.js"></script>-->
+<script src="js/jquery-3.2.0.js"></script>
+<script src="js/jquery-ui.min.js"></script>
 <script src="lib/bootstrap/popper.min.js"></script>
 <script src="lib/bootstrap/bootstrap.min.js"></script>
 <script src="lib/dataTables/jquery.dataTables.min.js"></script>
