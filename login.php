b88ba0f7cbed9147c78dcfaca6c1b49a7e311ef0
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-12 15:02:23 -0500

    Update login.php

--- login.php
+++ login.php
@@ -4,9 +4,27 @@
         <link rel="icon" href="images/igg.png" type="image/png" />
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1" />  
 		<title>Pandora</title>
-        <?php
-            include("libreriasindex.php");
-        ?>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
+        <link rel="stylesheet" type="text/css" href="js/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="lib/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="lib/dataTables/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="lib/dataTables/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" type="text/css" href="lib/alertifyjs/css/alertify.css">
+        <link rel="stylesheet" type="text/css" href="lib/alertifyjs/css/themes/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="lib/fontawesome/css/fontawesome.css">
+        <link rel="stylesheet" type="text/css" href="lib/fontawesome/css/all.css">  
+        <link rel="stylesheet" type="text/css" href="css/estilos_index.css">
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="lib/bootstrap/popper.min.js"></script>
+        <script src="lib/bootstrap/bootstrap.min.js"></script>
+        <script src="lib/dataTables/jquery.dataTables.min.js"></script>
+        <script src="lib/dataTables/dataTables.bootstrap4.min.js"></script>
+        <script src="lib/alertifyjs/alertify.js"></script>
+        <script src="js/moment.min.js"></script>
+
+		<script src="js/logicas_login.js"></script>
 	</head>
 	<body class="fondoOficial">
         <div id="loading">
