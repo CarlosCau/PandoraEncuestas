b54421fe6ff0985c4b334a4d538674374467d84f
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-11 21:16:11 -0500

    Update display_login.php

--- display_login.php
+++ display_login.php
@@ -1,3 +1,11 @@
+<!--
+<?php 
+session_start();
+if($_SESSION["log"] !== "sesion_on"){
+	echo"<meta http-equiv=refresh content=0.1;url=http://gestion.upaxer.com/index.php>";
+}
+?>
+-->
 <?php
     session_start();
     require_once "libreriasindex.php";
