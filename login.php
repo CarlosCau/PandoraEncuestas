affa1de0a3a65bfa031703ca23f95776bf908cca
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-11 21:22:11 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
+<!--
+<?php 
+session_start();
+if($_SESSION["log"] !== "sesion_on"){
+	echo"<meta http-equiv=refresh content=0.1;url=http://gestion.upaxer.com/index.php>";
+}
+?>
+-->
 <?php
     require_once "display_login.php";
 ?>
