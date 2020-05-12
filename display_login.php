e0703ca97bd4ee64ea89428495d26d05bd3479ca
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-12 00:50:20 -0500

    Update display_login.php

--- display_login.php
+++ display_login.php
@@ -15,7 +15,7 @@ if($_SESSION["log"] !== "sesion_on"){
 <title>Pandora</title>
 
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
-<link rel="stylesheet" type="text/css" href="js/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="js/jquery-ui.min.css">
 <link rel="stylesheet" type="text/css" href="lib/bootstrap/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="lib/dataTables/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="lib/dataTables/dataTables.bootstrap4.min.css">
@@ -26,9 +26,11 @@ if($_SESSION["log"] !== "sesion_on"){
 <link rel="stylesheet" type="text/css" href="css/estilos_index.css">
 
 
-<script src="js/jquery-1.10.2.js"></script> <!--para autocomplete-->
-<script src="js/jquery-ui.js"></script> <!--para autocomplete-->  
+<!--<script src="js/jquery-1.10.2.js"></script> para autocomplete-->
+<!--<script src="js/jquery-ui.js"></script> para autocomplete-->  
 <!--<script src="js/jquery.min.js"></script>-->
+<script src="js/jquery-3.2.0.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
 <script src="lib/bootstrap/popper.min.js"></script>
 <script src="lib/bootstrap/bootstrap.min.js"></script>
 <script src="lib/dataTables/jquery.dataTables.min.js"></script>
