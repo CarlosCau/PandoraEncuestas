cbd0385a2739feca5d88ffca630e3457f1b0d140
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-05-13 03:26:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -37,9 +37,9 @@
                     </div>
                 </div>
             </nav>
-            <div class="col-sm-12 col-lg-12 borde divbody">
+            <div class="borde divbody">
                 <div id="contLogos" class="col-sm-12 col-lg-12 borde">
-                        <div class="col-sm-12 col-lg-8 ">
+                        <div id="contenedorImg" class="col-sm-12 col-lg-12">
                             <img src="images/IG.png" class="logos borde">
                             <img src="images/LogoAO.png" class="logos borde">
                         </div>
@@ -47,12 +47,10 @@
                 <div id="textos">
                     <div class="row">
                         <div class="col-sm-12 col-lg-12 borde">
-                            <p class="tituloBody borde">Percepción Local del Riesgo y Capacidad de Respuesta, Álvaro Obregón</p>
+                            <p class="tituloBody borde">Percepción Local del Riesgo, Álvaro Obregón</p>
                         </div>
                         <div class="col-sm-12 col-lg-12 borde">
-                            <p class=" parrafo borde">Consta de un cuestionario dividido en dos partes:</p>
-                            <p class=" parrafo borde">1.- Se refiere a la percepción local de riesgo, es decir, el imaginario colectivo que tiene la población acerca de las susceptibles que existen en su comunidad y de su grado de exposición frente a las mismas</p>
-                            <p class=" parrafo borde">2. Se enfoca en la capacidad de prevención y de respuesta, la cual se refiere a la preparación antes y después de un evento de las autoridades y de la población, se aplica mediante cuestionario.</p>
+                            <p class=" parrafo borde">Aplicación para levantamiento de Información en campo de la percepción local de riesgo, es decir, el imaginario colectivo que tiene la población acerca de las susceptibilidades que existen en su comunidad y de su grado de exposición frente a las mismas.</p>
                         </div>
                         <div id="boton" class="col-sm-12 col-lg-12">
                             <button id="btnIngresar" class="btn btn-primary centrado btn_ingresar" onclick="display_login.php">Ingresar</button>
